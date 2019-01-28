4fddf22f058477eb573aa2e3c18b5e8bdc27ea21
OE16 - R - Phan Van Vuong <vuongphan139.it@gmail.com>
2019-01-28 11:22:59 +0700

    task 6026: user_signin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_25_094122) do
+ActiveRecord::Schema.define(version: 2019_01_26_223148) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
