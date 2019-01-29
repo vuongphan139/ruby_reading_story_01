6bdbe14fe6f9b544b28edadd1b36fee284c9af59
Duc Nam <ducnam0807@gmail.com>
2019-01-29 10:04:52 +0700

    log_out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new user_params
+
     if @user.save
+      log_in @user
       flash[:success] = t "sign_up_success"
       redirect_to root_path
     else
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -34,4 +34,10 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
+
+  def log_out
+    session.delete :user_id
+    @current_user = nil
+  end
+
 end
